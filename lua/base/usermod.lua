1c481093c69744e94d9fd36a6d378cf04e5d903a
ariefra <arief.r.id@gmail.com>
2025-01-08 04:36:27 +0700

    specify theme in user/custom.lua

--- lua/base/usermod.lua
+++ lua/base/usermod.lua
@@ -1,19 +1,17 @@
 local userpath = vim.fn.stdpath 'config' .. '/lua/user'
 if not vim.loop.fs_stat(userpath) then
-  print 'creating customization template ...'
-  vim.fn.mkdir(userpath, 'p')
-  vim.fn.writefile(
-    {
-      '-- custom init code here',
-      'local hour = tonumber(os.date("%H"))',
-      'if hour >= 6 and hour < 18 then vim.o.bg = "light" else vim.o.bg = "dark" end',
-      'return {',
-      '{ "https://github.com/nyoom-engineering/oxocarbon.nvim.git", },',
-      '-- custom plugins spec here',
-      '}'
-    },
-    userpath .. '/custom.lua'
-  )
-  return {}
+	print 'creating customization template ...'
+	vim.fn.mkdir(userpath, 'p')
+	vim.fn.writefile({
+		'-- custom init code here',
+		'local hour = tonumber(os.date("%H"))',
+		'if hour >= 6 and hour < 18 then vim.o.bg = "light" else vim.o.bg = "dark" end',
+		'vim.g.colors_name = "oxocarbon"  -- see themes.lua for list of themes',
+		'return {',
+		'{ "https://github.com/nyoom-engineering/oxocarbon.nvim.git",},',
+		'{-- custom plugins spec here}',
+		'}',
+	}, userpath .. '/custom.lua')
+	return {}
 end
 return { import = 'user/custom' }
