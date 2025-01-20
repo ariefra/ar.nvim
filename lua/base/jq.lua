d48b883a56964be459906419f42423907e479ff1
ariefra <arief.r.id@gmail.com>
2025-01-20 21:02:00 +0700

    re-enable default oxocarbon theme

---
+++ lua/base/jq.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		'https://github.com/yochem/jq-playground.nvim',
+		keys = {
+			{ '<leader>jq', '<cmd>JqPlayground<cr>', desc = 'jq-playground' },
+		},
+		ft = 'json',
+		cmd = 'JqPlayground',
+	},
+}
--- lua/base/options.lua
+++ lua/base/options.lua
@@ -44,6 +44,9 @@ g.loaded_python3_provider = 0
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
+
 -- g.autolight = true
--- g.colors_name = 'oxocarbon'
-return {}
+g.colors_name = 'oxocarbon'
+return {
+  { 'https://github.com/nyoom-engineering/oxocarbon.nvim.git', },
+}
